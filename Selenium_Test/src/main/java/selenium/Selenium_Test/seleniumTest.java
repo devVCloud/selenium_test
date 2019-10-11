81c6f6f00edfcb436652712712413e545af4b93a
deVCloud <wa.01.ch@gmail.com>
2019-10-11 09:19:03 +0900

    selenium

---
+++ Selenium_Test/src/main/java/selenium/Selenium_Test/seleniumTest.java
@@ -0,0 +1,13 @@
+package selenium.Selenium_Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class seleniumTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
